bd5fa9859ecd05688b4315faa34f70d8e4eb3361
Sergeev1745 <sergeev.be1@gmail.com>
2016-10-26 21:57:00 +0300

    fixed

--- js/galery.js
+++ js/galery.js
@@ -2,10 +2,9 @@ jQuery(document).ready(function(){
 var mainImgHref = $("#zoom_01").attr("src");
 
 $(".small-pic-row > img").click(function(){
-	var href = $(this).attr("href");
+	var href = $(this).attr("src");
 	mainImgHref = href;
-	$("#zoom_01").attr("src", href);
-	$("#zoom_01").attr("data-zoom-image", href);
+	$("#zoom_01").attr("src", href).attr("data-zoom-image", href);	
 	$(".small-pic-row > img").removeClass("activ-img");
 	$(this).addClass("activ-img");	
 	$(".zoomWindow").css('background-image','url(' + href + ')');
@@ -14,7 +13,7 @@ $(".small-pic-row > img").click(function(){
 	$("#zoom_01").attr("src", mainImgHref);
 });
 	$(".small-pic-row > img").mouseover(function(){
-	var transitory = $(this).attr("href");
+	var transitory = $(this).attr("src");
 	$("#zoom_01").attr("src", transitory);
 	
 });
